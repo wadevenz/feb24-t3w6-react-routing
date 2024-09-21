e8a1f53adab918b4784bcb20e08d90fab4109219
Wade <158378370+wadevenz@users.noreply.github.com>
2024-09-21 12:03:51 +1000

    feat: nested route

--- src/components/App_ComponentRouter.js
+++ src/components/App_ComponentRouter.js
@@ -5,6 +5,8 @@ import HomePage from '../pages/Homepage';
 import ContactPage from '../pages/ContactPage';
 import AboutPage from '../pages/AboutPage';
 import Navbar from './Navbar';
+import ProjectLayout from '../pages/_templates/ProjectLayout';
+import PokemonProject from '../pages/projects/PokemonProject';
 
 
 function App() {
@@ -19,6 +21,8 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contact" element={<ContactPage />} />
+          <Route path="/projects" element={<ProjectLayout />} />
+            <Route path="/projects/pokemon" element={<PokemonProject />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/projects/PokemonProject.jsx
@@ -0,0 +1,14 @@
+
+export default function PokemonProject(){
+    return(
+        <div>
+            <h1>Pokemon Project</h1>
+            <p>Here is a list of Pokemon:</p>
+            <ul>
+                <li>Bulbasaur</li>
+                <li>Squirtle</li>
+                <li>Charmander</li>
+            </ul>
+        </div>
+    )
+}
