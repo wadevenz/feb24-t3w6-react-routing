3b6cc3688a5a1c9f321d868b9aeeaf812487f659
Wade <158378370+wadevenz@users.noreply.github.com>
2024-09-21 11:44:37 +1000

    feat: redirect button

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,9 +1,12 @@
+import Redirectbutton from "../components/Redirectbutton";
 
 export default function ContactPage(){
 
     return(
         <div>
             <h1>Contact</h1>
+
+            <Redirectbutton />
         </div>
     )
 }
